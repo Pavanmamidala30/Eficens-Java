43a697d25708b9606223454f3825395aba582522
User <pavankumar.mamidala20@gmail.com>
2023-06-03 10:11:42 -0500

    First Commit

---
+++ WeekendPractice/src/main/java/com/eficens/practice/Samplecode.java
@@ -0,0 +1,12 @@
+package com.eficens.practice;
+
+public class Samplecode {
+
+    public static int addTwoInt(Integer a, Integer b){
+        int c=a+b;
+        return c;
+    }
+    public static void main(String[] args) {
+
+    }
+}
