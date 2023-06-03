b47d8147f921d7273d0d8757bacc59c13f181100
User <pavankumar.mamidala20@gmail.com>
2023-06-03 10:37:33 -0500

    Correction

--- WeekendPractice/src/main/java/com/eficens/practice/Samplecode.java
+++ WeekendPractice/src/main/java/com/eficens/practice/Samplecode.java
@@ -2,12 +2,12 @@ package com.eficens.practice;
 
 public class Samplecode {
 
-    public static int addTwoInt(Integer a, Integer b){
+    public static int addTwoInt(int a, int b){
         int c=a+b;
         return c;
     }
 
-    public static int subTwoInt(Integer a, Integer b){
+    public static int subTwoInt(int a, int b){
         int c = a-b;
         return c;
     }
